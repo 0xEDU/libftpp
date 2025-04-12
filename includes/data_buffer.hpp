dec366e956c932b8240deb3a4fe5fb3f1d0a66a1
edu <bfwaend@gmail.com>
2025-04-12 12:39:10 -0300

    Finish DataBuffer class

--- includes/data_buffer.hpp
+++ includes/data_buffer.hpp
@@ -1,7 +1,33 @@
+#include <cstdint>
+#include <cstring>
+#include <stdexcept>
+#include <vector>
+
 class DataBuffer {
-	public:
-		DataBuffer();
-		DataBuffer(const DataBuffer &);            // Prevent copying
-		DataBuffer &operator=(const DataBuffer &); // Prevent assignment
-		~DataBuffer();
+	std::vector<uint8_t> dataBuffer;
+public:
+  DataBuffer();
+  DataBuffer(const DataBuffer &);
+  DataBuffer &operator=(const DataBuffer &);
+  ~DataBuffer();
+
+	template<typename T>
+	DataBuffer &operator<<(const T& object) {
+		const uint8_t *data = reinterpret_cast<const uint8_t*>(&object);
+		dataBuffer.insert(dataBuffer.end(), data, data + sizeof(T));
+		return *this;
+	}
+
+	template<typename T>
+	DataBuffer &operator>>(T& object) {
+		if (dataBuffer.size() < sizeof(T)) {
+			throw std::runtime_error("Not enough data to deserialize");
+		}
+		std::memcpy(&object, dataBuffer.data(), sizeof(T));
+		dataBuffer.erase(dataBuffer.begin(), dataBuffer.begin() + sizeof(T));
+		return *this;
+	};
+
+	DataBuffer &operator<<(const std::string &str);
+	DataBuffer &operator>>(std::string &str);
 };
--- sources/data_buffer.cpp
+++ sources/data_buffer.cpp
@@ -11,3 +11,23 @@ DataBuffer &DataBuffer::operator=(const DataBuffer &rhs) {
 	if (this != &rhs) {}
 	return *this;
 }
+
+// std::string is weird
+DataBuffer &DataBuffer::operator<<(const std::string &str) {
+	size_t length = str.size();
+	*this << length;
+	dataBuffer.insert(dataBuffer.end(), str.begin(), str.end());
+	return *this;
+}
+
+DataBuffer &DataBuffer::operator>>(std::string &str) {
+	size_t length;
+	*this >> length;
+
+	if (dataBuffer.size() < length) {
+		throw std::runtime_error("Not enough data to deserialize string");
+	}
+	str.assign(reinterpret_cast<const char *>(dataBuffer.data()), length);
+	dataBuffer.erase(dataBuffer.begin(), dataBuffer.begin() + length);
+	return *this;
+}
