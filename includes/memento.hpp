f3b14409798b5ecf8688c9cb45af7455e5dbf477
edu <bfwaend@gmail.com>
2025-04-13 01:00:42 -0300

    Finish Memento ~mori~

--- includes/memento.hpp
+++ includes/memento.hpp
@@ -1,8 +1,44 @@
 #ifndef MEMENTO_HPP
 #define MEMENTO_HPP
 
-class Memento {
+#include "data_buffer.hpp"
 
+class Memento {
+protected:
+  Memento();
+  Memento(const Memento &);
+  Memento &operator=(const Memento &);
+  ~Memento();
+
+public:
+  class Snapshot {
+    friend class Memento;
+
+  private:
+    DataBuffer dataBuffer;
+
+  public:
+    Snapshot();
+    Snapshot(const Snapshot &);
+    Snapshot &operator=(const Snapshot &);
+    ~Snapshot();
+
+    template <typename T> Snapshot &operator<<(const T &object) {
+      dataBuffer << object;
+      return *this;
+    };
+
+    template <typename T> Snapshot &operator>>(T &object) {
+      dataBuffer >> object;
+      return *this;
+    };
+  };
+
+  Snapshot save();
+  void load(const Snapshot &state);
+
+  virtual void _saveToSnapshot(Snapshot &snapshotToFill) = 0;
+  virtual void _loadFromSnapshot(Snapshot &snapshot) = 0;
 };
 
 #endif // !MEMENTO_HPP
---
+++ sources/memento.cpp
@@ -0,0 +1,29 @@
+#include "../includes/memento.hpp"
+
+using Snapshot = Memento::Snapshot;
+
+Memento::Memento() = default;
+Memento::Memento(const Memento &) = default;
+Memento &Memento::operator=(const Memento &) = default;
+Memento::~Memento() = default;
+
+Memento::Snapshot Memento::save() {
+	Snapshot snapshot;
+	_saveToSnapshot(snapshot);
+	return snapshot;
+};
+
+void Memento::load(const Snapshot &state) {
+	Snapshot snapshot(state);
+	_loadFromSnapshot(snapshot);
+};
+
+Snapshot::Snapshot() = default;
+Snapshot::Snapshot(const Snapshot &rhs) { *this = rhs; };
+Snapshot &Snapshot::operator=(const Snapshot &rhs) {
+	if (this != &rhs) {
+		dataBuffer = rhs.dataBuffer;
+	}
+	return *this;
+};
+Snapshot::~Snapshot() = default;
