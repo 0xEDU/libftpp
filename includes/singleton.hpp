b48844aaca010ed91664c16edee9fa416523bd0c
edu <bfwaend@gmail.com>
2025-04-14 12:56:37 -0300

    Format Singleton class

--- includes/singleton.hpp
+++ includes/singleton.hpp
@@ -4,32 +4,30 @@
 #include <memory>
 #include <stdexcept>
 
-template <typename TType>
-class Singleton {
-	static std::unique_ptr<TType> instancePtr;
+template <typename TType> class Singleton {
+  static std::unique_ptr<TType> instancePtr;
 
 public:
-	// Delete copy constructor and assignment operator
-	Singleton() = default;
-	Singleton(const Singleton&) = delete;
-	Singleton& operator=(const Singleton&) = delete;
-	~Singleton() = default;
+  // Delete copy constructor and assignment operator
+  Singleton() = default;
+  Singleton(const Singleton &) = delete;
+  Singleton &operator=(const Singleton &) = delete;
+  ~Singleton() = default;
 
-	static TType* instance() {
-		if (instancePtr == nullptr) {
-			throw std::runtime_error("Instance not yet created");
-		}
-		return instancePtr.get();
-	}
+  static TType *instance() {
+    if (instancePtr == nullptr) {
+      throw std::runtime_error("Instance not yet created");
+    }
+    return instancePtr.get();
+  }
 
-	template<typename... TArgs>
-	static void instantiate(TArgs&&... p_args) {
-		if (instancePtr != nullptr) {
-			throw std::runtime_error("Instance already created");
-		}
+  template <typename... TArgs> static void instantiate(TArgs &&...p_args) {
+    if (instancePtr != nullptr) {
+      throw std::runtime_error("Instance already created");
+    }
 
-		instancePtr = std::make_unique<TType>(p_args...);
-	}
+    instancePtr = std::make_unique<TType>(p_args...);
+  }
 };
 
 template <typename TType>
