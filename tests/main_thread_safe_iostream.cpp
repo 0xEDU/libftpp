feaccb655423a3b822b2cbbe31dc093b0a599dde
edu <bfwaend@gmail.com>
2025-04-17 21:49:57 -0300

    Test prompt method in thread_safe_iostream

--- tests/main_thread_safe_iostream.cpp
+++ tests/main_thread_safe_iostream.cpp
@@ -2,11 +2,17 @@
 #include <thread>
 #include "thread_safe_iostream.hpp"
 
+std::mutex ThreadSafeIOStream::mtx;
+thread_local ThreadSafeIOStream threadSafeCout;
+
 void printNumbers(const std::string& p_prefix) {
     threadSafeCout.setPrefix(p_prefix);
     for (int i = 1; i <= 5; ++i) {
         threadSafeCout << "Number: " << i << std::endl;
     }
+	std::string answer;
+	threadSafeCout.prompt("How are you? ", answer);
+	threadSafeCout << answer << std::endl;
 }
 
 int main() {
