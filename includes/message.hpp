21b2c298564399c03f117d60e03c670bc09a8fda
Eduardo Tachotte <bfwaend@gmail.com>
2025-05-04 12:22:07 -0300

    Fix << and >> implementation and Type

--- includes/message.hpp
+++ includes/message.hpp
@@ -2,14 +2,14 @@
 #define MESSAGE_HPP
 
 #include "../includes/data_buffer.hpp"
+#include "../includes/thread_safe_iostream.hpp"
 
 class Message {
-	using Type = int;
-
-	Type m_type;
+	int m_type;
 	DataBuffer m_dataBuffer;
 
 public:
+	using Type = int;
 
 	Message() = default;
 	Message(const Message&);
@@ -20,16 +20,22 @@ public:
 	int type();
 
 	template<typename T>
-	DataBuffer& operator<<(const T& object) {
-		m_dataBuffer << object;
-		return m_dataBuffer;
-	}
+	friend const Message& operator<<(const Message&, const T& object);
 
 	template<typename T>
-	DataBuffer& operator>>(T& object) {
-		m_dataBuffer >> object;
-		return m_dataBuffer;
-	}
+	friend const Message& operator>>(const Message&, T& object);
 };
+
+template<typename T>
+const Message& operator<<(const Message& msg, const T& object) {
+	msg.m_dataBuffer << object;
+	return msg;
+}
+
+template<typename T>
+const Message& operator>>(const Message& msg, T& object) {
+	msg.m_dataBuffer >> object;
+	return msg;
+}
 
 #endif // !MESSAGE_HPP
