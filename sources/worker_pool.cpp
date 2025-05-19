31c6c73c618eb1827ad0e29128579eaffcc11f92
edu <bfwaend@gmail.com>
2025-05-19 00:45:34 -0300

    Format WorkerPool

--- sources/worker_pool.cpp
+++ sources/worker_pool.cpp
@@ -3,48 +3,47 @@
 std::mutex WorkerPool::haltMutex;
 
 WorkerPool::WorkerPool() = default;
-WorkerPool::WorkerPool(const WorkerPool& rhs) {
-	*this = rhs;
-}
-WorkerPool& WorkerPool::operator=(const WorkerPool& rhs) {
-	if (this != &rhs) {
-	}
-	return *this;
+WorkerPool::WorkerPool(const WorkerPool &rhs) { *this = rhs; }
+WorkerPool &WorkerPool::operator=(const WorkerPool &rhs) {
+  if (this != &rhs) {
+  }
+  return *this;
 };
 WorkerPool::~WorkerPool() {
-	{
-		std::lock_guard<std::mutex> lock(haltMutex);
-		halt = true;
-	}
-	for (auto& thread : threads) {
-		thread->stop();
-	}
+  {
+    std::lock_guard<std::mutex> lock(haltMutex);
+    halt = true;
+  }
+  for (auto &thread : threads) {
+    thread->stop();
+  }
 }
 
 WorkerPool::WorkerPool(int numWorkers) {
-	threads.resize(numWorkers);
+  threads.resize(numWorkers);
 
-	for (int i = 0; i < numWorkers; ++i) {
-		threads[i] = std::make_unique<Thread>("Worker " + std::to_string(i), [this]() {
-			while (true) {
-				try {
-					auto job = jobsQueue.pop_front();
-					job();
-				} catch (const std::exception& e) {
-					std::this_thread::sleep_for(std::chrono::milliseconds(100));
-				}
-				{
-					std::lock_guard<std::mutex> lock(haltMutex);
-					if (halt) {
-						break;
-					}
-				}
-			}
-		});
-		threads[i]->start();
-	}
+  for (int i = 0; i < numWorkers; ++i) {
+    threads[i] =
+        std::make_unique<Thread>("Worker " + std::to_string(i), [this]() {
+          while (true) {
+            try {
+              auto job = jobsQueue.pop_front();
+              job();
+            } catch (const std::exception &e) {
+              std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            }
+            {
+              std::lock_guard<std::mutex> lock(haltMutex);
+              if (halt) {
+                break;
+              }
+            }
+          }
+        });
+    threads[i]->start();
+  }
 }
 
-void WorkerPool::addJob(const std::function<void()>& jobToExecute) {
-	jobsQueue.push_back(jobToExecute);
+void WorkerPool::addJob(const std::function<void()> &jobToExecute) {
+  jobsQueue.push_back(jobToExecute);
 }
