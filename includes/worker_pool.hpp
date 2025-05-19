836e5e787657223990ed33bbe09f1eefa08bd248
edu <bfwaend@gmail.com>
2025-05-19 00:36:46 -0300

    Format WorkerPool

--- includes/worker_pool.hpp
+++ includes/worker_pool.hpp
@@ -1,29 +1,30 @@
 #ifndef WORKER_POOL_HPP
 #define WORKER_POOL_HPP
 
-#include <functional>
-#include "../includes/thread_safe_queue.hpp"
 #include "../includes/thread.hpp"
+#include "../includes/thread_safe_queue.hpp"
+#include <functional>
+#include <vector>
 
 class WorkerPool {
-	static std::mutex haltMutex;
-	bool halt = false;
-	ThreadSafeQueue<std::function<void()>> jobsQueue;
-	std::vector<std::unique_ptr<Thread>> threads;
+  static std::mutex haltMutex;
+  bool halt = false;
+  ThreadSafeQueue<std::function<void()>> jobsQueue;
+  std::vector<std::unique_ptr<Thread>> threads;
 
 public:
-	WorkerPool();
-	WorkerPool(const WorkerPool&);
-	WorkerPool& operator=(const WorkerPool&);
-	~WorkerPool();
+  WorkerPool();
+  WorkerPool(const WorkerPool &);
+  WorkerPool &operator=(const WorkerPool &);
+  ~WorkerPool();
 
-	WorkerPool(int numWorkers);
-	void addJob(const std::function<void()>& jobToExecute);
+  WorkerPool(int numWorkers);
+  void addJob(const std::function<void()> &jobToExecute);
 
-	class IJobs {
-	public:
-		virtual ~IJobs() = default;
-	};
+  class IJobs {
+  public:
+    virtual ~IJobs() = default;
+  };
 };
 
 #endif // !WORKER_POOL_HPP
