ee564e8d535f1ebd613db7a6b7cecfc11af5976a
Eduardo Tachotte <bfwaend@gmail.com>
2025-05-21 18:52:06 +0000

    Update global header

--- includes/libftpp.hpp
+++ includes/libftpp.hpp
@@ -1,6 +1,11 @@
 #ifndef LIBFTPP_HPP
 #define LIBFTPP_HPP
 
-#include "pool.hpp"
+#include "data_structures.hpp"
+#include "design_patterns.hpp"
+#include "mathematics.hpp"
+#include "network.hpp"
+#include "thread_safe_iostream.hpp"
+#include "threading.hpp"
 
 #endif // !LIBFTPP_HPP
