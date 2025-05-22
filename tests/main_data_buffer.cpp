5f7b5001d6b029b1de2d2acbe95b0f7b99ec807f
edu <bfwaend@gmail.com>
2025-05-22 15:46:46 -0300

    Test DataBuffer properly

--- tests/main_data_buffer.cpp
+++ tests/main_data_buffer.cpp
@@ -1,4 +1,6 @@
-#include "data_buffer.hpp" // Assuming your DataBuffer is defined in this header
+#include "../includes/data_buffer.hpp"
+#include "../includes/test_helper.hpp"
+#include <cassert>
 #include <exception>
 #include <iostream>
 #include <string>
@@ -21,6 +23,7 @@ public:
 };
 
 int main() {
+  TestHelper testHelper;
   DataBuffer myBuffer;
 
   TestObject obj1;
@@ -32,30 +35,28 @@ int main() {
   obj2.y = "World";
 
   myBuffer << obj1 << obj2;
+  testHelper.expectTrue(myBuffer.data() != nullptr,
+                        "Buffer should not be null after serialization");
 
   TestObject deserializedObj1, deserializedObj2, deserializedObj3;
 
-  // This should work as expected
-  try {
-    myBuffer >> deserializedObj1 >> deserializedObj2;
-    std::cout << "Deserialized obj1: x = " << deserializedObj1.x
-              << ", y = " << deserializedObj1.y << '\n';
-    std::cout << "Deserialized obj2: x = " << deserializedObj2.x
-              << ", y = " << deserializedObj2.y << '\n';
-  } catch (const std::exception &e) {
-    std::cout << "Caught exception: " << e.what() << '\n';
-  }
+  testHelper.expectNotThrow(
+      [&]() {
+        myBuffer >> deserializedObj1 >> deserializedObj2;
+        std::cout << "Deserialized obj1: x = " << deserializedObj1.x
+                  << ", y = " << deserializedObj1.y << '\n';
+        std::cout << "Deserialized obj2: x = " << deserializedObj2.x
+                  << ", y = " << deserializedObj2.y << '\n';
+      },
+      "Deserialization should not throw an exception");
 
-  // This should throw an exception because there are no more objects to
-  // deserialize
-  try {
-    myBuffer >> deserializedObj3;
-    std::cout << "Deserialized obj3: x = " << deserializedObj3.x
-              << ", y = " << deserializedObj3.y << '\n';
-  } catch (const std::exception &e) {
-    std::cout << "Caught exception: " << e.what()
-              << '\n'; // This line should be executed
-  }
+  testHelper.expectThrow(
+      [&]() {
+        myBuffer >> deserializedObj3;
+        std::cout << "Deserialized obj3: x = " << deserializedObj3.x
+                  << ", y = " << deserializedObj3.y << '\n';
+      },
+      "Deserialization should throw an exception");
 
   return 0;
 }
