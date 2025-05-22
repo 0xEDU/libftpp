a73bacfce2cc3f5614d1816dfdc3172feb63f7cc
edu <bfwaend@gmail.com>
2025-05-22 15:47:31 -0300

    Test Chronometer

--- tests/main_chronometer.cpp
+++ tests/main_chronometer.cpp
@@ -1,16 +1,20 @@
 #include "../includes/chronometer.hpp"
+#include "../includes/test_helper.hpp"
 #include <iostream>
 #include <thread>
 
 int main() {
+  TestHelper testHelper;
   Chronometer chrono;
 
   std::cout << "Starting chronometer...\n";
   chrono.start();
+  testHelper.expectTrue(chrono.isRunning(), "Chronometer should be running");
 
   std::this_thread::sleep_for(std::chrono::milliseconds(1500)); // simulate work
 
   chrono.stop();
+  testHelper.expectFalse(chrono.isRunning(), "Chronometer should not be running");
   std::cout << "Elapsed time: " << chrono.elapsedMilliseconds() << " ms\n";
   std::cout << "Elapsed time: " << chrono.elapsedSeconds() << " s\n";
 
