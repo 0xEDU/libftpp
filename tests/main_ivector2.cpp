ec9b0463835d492d64d34581b9dbc007264518f0
edu <bfwaend@gmail.com>
2025-05-22 15:51:01 -0300

    Test IVector2

--- tests/main_ivector2.cpp
+++ tests/main_ivector2.cpp
@@ -1,7 +1,9 @@
 #include "../includes/ivector2.hpp"
+#include "../includes/test_helper.hpp"
 #include <iostream>
 
 int main() {
+  TestHelper testHelper;
   IVector2<int> vec1(3, 4);
   IVector2<int> vec2(1, 2);
   IVector2<float> vec3(3.0, 4.0);
@@ -12,47 +14,56 @@ int main() {
   // Test operator overloads
   auto vecAdd = vec1 + vec2;
   std::cout << "vec1 + vec2 = (" << vecAdd.x << ", " << vecAdd.y << ")" << '\n';
-  // Expected: vec1 + vec2 = (4, 6)
+  testHelper.expectTrue(vecAdd == IVector2<int>(4, 6),
+                        "vec1 + vec2 should equal (4, 6)");
 
   auto vecSub = vec1 - vec2;
   std::cout << "vec1 - vec2 = (" << vecSub.x << ", " << vecSub.y << ")" << '\n';
-  // Expected: vec1 - vec2 = (2, 2)
+  testHelper.expectTrue(vecSub == IVector2<int>(2, 2),
+                        "vec1 - vec2 should equal (2, 2)");
 
   auto vecMul = vec1 * vec2;
   std::cout << "vec1 * vec2 = (" << vecMul.x << ", " << vecMul.y << ")" << '\n';
-  // Expected: vec1 * vec2 = (3, 8)
+  testHelper.expectTrue(vecMul == IVector2<int>(3, 8),
+                        "vec1 * vec2 should equal (3, 8)");
 
   auto vecDiv = vec1 / vec2;
   std::cout << "vec1 / vec2 = (" << vecDiv.x << ", " << vecDiv.y << ")" << '\n';
-  // Expected: vec1 / vec2 = (3, 2)
+  testHelper.expectTrue(vecDiv == IVector2<int>(3, 2),
+                        "vec1 / vec2 should equal (3, 2)");
 
   bool isEqual = vec1 == vec2;
   std::cout << "vec1 == vec2: " << (isEqual ? "true" : "false") << "" << '\n';
-  // Expected: vec1 == vec2: false
+  testHelper.expectFalse(isEqual, "vec1 == vec2 should be false");
 
   bool isNotEqual = vec1 != vec2;
   std::cout << "vec1 != vec2: " << (isNotEqual ? "true" : "false") << ""
             << '\n';
-  // Expected: vec1 != vec2: true
+  testHelper.expectTrue(isNotEqual, "vec1 != vec2 should be true");
 
   // Test additional methods
   float len = vec1.length();
   std::cout << "Length of vec1: " << len << "" << '\n';
-  // Expected: Length of vec1: 5 (or sqrt(3*3 + 4*4))
+  testHelper.expectTrue(len == 5,
+                        "Length of vec1 should be 5 (or sqrt(3*3 + 4*4))");
 
   auto normVec = vec3.normalize();
   std::cout << "Normalized vec1 = (" << normVec.x << ", " << normVec.y << ")"
             << '\n';
-  // Expected: Normalized vec1 = (0.6, 0.8)
+  testHelper.expectTrue(normVec == IVector2<float>(0.6, 0.8),
+                        "Normalized vec1 should be (0.6, 0.8)");
 
   float dotProd = vec1.dot(vec2);
   std::cout << "Dot product of vec1 and vec2: " << dotProd << "" << '\n';
-  // Expected: Dot product of vec1 and vec2: 11 (or 3*1 + 4*2)
+  testHelper.expectTrue(
+      dotProd == 11,
+      "Dot product of vec1 and vec2 should be 11 (or 3*1 + 4*2)");
 
   auto crossProd = vec1.cross(vec2);
   std::cout << "Cross product of vec1: (" << crossProd.x << ", " << crossProd.y
             << ")" << '\n';
-  // Expected: Cross product of vec1: (2, -2)
+  testHelper.expectTrue(crossProd == IVector2<int>(2, -2),
+                        "Cross product of vec1 should be (2, -2)");
 
   return 0;
 }
